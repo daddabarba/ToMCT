a16296daf0118395760d9f2102194e2e00feaedc
daddabarba <davidebarbieri97@gmail.com>
2018-10-11 22:25:22 +0200

    added sender to messages

--- src/ToMCT/Model/Messages/Message.java
+++ src/ToMCT/Model/Messages/Message.java
@@ -1,16 +1,34 @@
 package ToMCT.Model.Messages;
 
+import java.util.Observable;
+
 public class Message<T> {
 
     private T content;
     private Object context;
 
-    public Message(T content){
-        this(content, null);
+    private Observable sender;
+
+    public Message(Observable sender, T content){
+        this(sender, content, null);
     }
 
-    public Message(T content, Object context){
+    public Message(Observable sender, T content, Object context){
         this.content = content;
         this.context = context;
+
+        this.sender = sender;
+    }
+
+    public T getContent(){
+        return content;
+    }
+
+    public Object getContext(){
+        return context;
+    }
+
+    public Observable getSender(){
+        return sender;
     }
 }

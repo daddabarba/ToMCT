3c3a833994e847294043398b357bd5ef08a89d40
daddabarba <davidebarbieri97@gmail.com>
2018-10-11 22:25:45 +0200

    implemented MessageBox

--- src/ToMCT/Model/Messages/MessageBox.java
+++ src/ToMCT/Model/Messages/MessageBox.java
@@ -1,4 +1,23 @@
 package ToMCT.Model.Messages;
 
-public class MessageBox {
+import java.util.Hashtable;
+import java.util.Observable;
+import java.util.Observer;
+
+public class MessageBox extends Observable {
+
+    public void notifyPlayers(Message<?> message){
+        setChanged();
+        notifyObservers(message);
+    }
+
+    public void notifyPlayers(Hashtable<? extends Observer, Message<?>> messages){
+
+        for(Observer observer : messages.keySet())
+            observer.update(this, messages.get(observer));
+
+    }
+
 }
+
+

278c25f3342de80eaa5ce3978660eee3ae4dd2d5
daddabarba <davidebarbieri97@gmail.com>
2018-10-12 01:34:35 +0200

    player backbone

--- src/ToMCT/Model/ColoredTrails/Agent/Player.java
+++ src/ToMCT/Model/ColoredTrails/Agent/Player.java
@@ -1,4 +1,13 @@
 package ToMCT.Model.ColoredTrails.Agent;
 
-public class Player {
+import java.util.Observable;
+import java.util.Observer;
+
+public class Player implements Observer {
+
+
+    @Override
+    public void update(Observable observable, Object message){
+        
+    }
 }

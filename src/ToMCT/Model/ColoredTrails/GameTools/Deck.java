619eb4dcfd34814bf769fb7e3fe01dbf9e1a2155
daddabarba <davidebarbieri97@gmail.com>
2018-10-11 22:01:06 +0200

    Deck definition

--- src/ToMCT/Model/ColoredTrails/GameTools/Deck.java
+++ src/ToMCT/Model/ColoredTrails/GameTools/Deck.java
@@ -1,4 +1,52 @@
 package ToMCT.Model.ColoredTrails.GameTools;
 
+import ToMCT.Model.ColoredTrails.Agent.Player;
+import ToMCT.Model.ColoredTrails.GameUtils.Chip;
+import ToMCT.Model.Messages.MessageBox;
+
+import java.util.Collection;
+import java.util.Hashtable;
+
 public class Deck {
+
+    private Collection<Player> players;
+    private Hashtable<Player, Chip[]> hands;
+
+    private MessageBox messageBox;
+
+    public Deck(MessageBox messageBox, Collection<Player> players){
+        this.players = players;
+        this.messageBox = messageBox;
+
+        this.resetHands();
+    }
+
+    public void shuffle(int handSize){
+        initHands(handSize);
+        notifyHands();
+    }
+
+    private void notifyHands(){
+        messageBox.notifyPlayers(hands);
+    }
+
+    private void initHands(int handSize){
+
+        for(Player player : players)
+            hands.put(player, generateHand(handSize));
+
+    }
+
+    private Chip[] generateHand(int handSize){
+        Chip[] hand = new Chip[handSize];
+
+        for(int i=0; i<handSize; i++)
+            hand[i] = Chip.randomChip();
+
+        return hand;
+    }
+
+    public void resetHands(){
+        this.hands = new Hashtable<>();
+    }
 }

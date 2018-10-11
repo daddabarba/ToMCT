aa8141815c38dd6f741f163821ac952a02374217
daddabarba <davidebarbieri97@gmail.com>
2018-10-11 23:06:44 +0200

    defined location class

--- src/ToMCT/Model/ColoredTrails/GameUtils/Location.java
+++ src/ToMCT/Model/ColoredTrails/GameUtils/Location.java
@@ -1,4 +1,34 @@
 package ToMCT.Model.ColoredTrails.GameUtils;
 
 public class Location {
+    private int x,y;
+
+    public Location(){
+        this(0,0);
+    }
+
+    public Location(int x, int y){
+        set(x,y);
+    }
+
+    public void set(int x, int y){
+        setX(x);
+        setY(y);
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.x = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
 }

f32b131ca0b303e38151a332c6b193fbe1769702
daddabarba <davidebarbieri97@gmail.com>
2018-10-11 22:14:49 +0200

    Messages defined

--- src/ToMCT/Model/Messages/Message.java
+++ src/ToMCT/Model/Messages/Message.java
@@ -1,4 +1,16 @@
 package ToMCT.Model.Messages;
 
-public class Message {
+public class Message<T> {
+
+    private T content;
+    private Object context;
+
+    public Message(T content){
+        this(content, null);
+    }
+
+    public Message(T content, Object context){
+        this.content = content;
+        this.context = context;
+    }
 }

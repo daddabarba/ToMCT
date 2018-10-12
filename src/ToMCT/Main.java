3f733c5997c46de09735088c74408cc9c8f06950
daddabarba <davidebarbieri97@gmail.com>
2018-10-12 19:59:10 +0200

    added testing main class

---
+++ src/ToMCT/Main.java
@@ -0,0 +1,11 @@
+package ToMCT;
+
+import ToMCT.Model.ColoredTrails.Game;
+import ToMCT.Model.ColoredTrails.GameUtils.Location;
+
+public class Main {
+
+    public static void main(String[] args){
+        Game game = new Game(3, 4, 5,5,new Location(2,2));
+    }
+}

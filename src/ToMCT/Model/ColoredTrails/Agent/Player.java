35c1fec19bbff0474fabb77a3d69a40bc8dcc9c0
daddabarba <davidebarbieri97@gmail.com>
2018-10-12 19:24:18 +0200

    added comments

--- src/ToMCT/Model/ColoredTrails/Agent/Player.java
+++ src/ToMCT/Model/ColoredTrails/Agent/Player.java
@@ -18,6 +18,7 @@ public class Player implements Observer {
 
     private Location goal, position; // The player's goal and current position
 
+    //CONSTRUCTOR
     public Player(){
         hand = new Hand();
     }
@@ -45,6 +46,8 @@ public class Player implements Observer {
     //NOTIFICATION HANDLER
 
     @Override
+
+    //Handles generic notification
     public void update(Observable observable, Object message){
 
         if( observable instanceof MessageBox && message instanceof Message )
@@ -52,26 +55,39 @@ public class Player implements Observer {
         
     }
 
+    //Handles notifications from message box
     private void handleUpdate(MessageBox messageBox, Message message){
 
+        //Classifies between notifications from Map
         if( message.getSender().getClass().equals(Map.class) )
             updateFromMap(message);
+        //Or from Deck
         else if( message.getSender().getClass().equals(Deck.class) )
             updateFromDeck(message);
 
     }
 
+    //Handles notification from Map
     private void updateFromMap(Message message){
 
+        //Identifies goal of the message
+
+        //If a location is sent with no context
         if(message.getContent() instanceof Location)
             if(message.getContext() == null)
+                //It must be the player's own location
                 setPosition(((Message<Location>)message).getContent());
     }
 
+    //Handles notifications from Deck
     private void updateFromDeck(Message message){
 
+        //Identifies goal message
+
+        //If a hand is sent with no context
         if(message.getContent() instanceof Hand)
             if(message.getContext() == null)
+                //It must be the player's own hand
                 setHand(((Message<Hand>)message).getContent());
     }
 

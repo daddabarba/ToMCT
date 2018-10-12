5d4dbd398473e871eaeff6b9387ddd01b678ab2f
daddabarba <davidebarbieri97@gmail.com>
2018-10-12 19:39:15 +0200

    backbone of Game class

--- src/ToMCT/Model/ColoredTrails/Game.java
+++ src/ToMCT/Model/ColoredTrails/Game.java
@@ -1,5 +1,52 @@
 package ToMCT.Model.ColoredTrails;
 
-public class Game {
+import ToMCT.Model.ColoredTrails.Agent.Player;
 
+import ToMCT.Model.ColoredTrails.GameTools.Deck;
+import ToMCT.Model.ColoredTrails.GameTools.Map;
+import ToMCT.Model.ColoredTrails.GameUtils.Location;
+import ToMCT.Model.Messages.MessageBox;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Observable;
+
+public class Game extends Observable {
+    //Class coordinating the different components of the CT game
+
+    private Collection<Player> players; //List of participants
+
+    private Map map; //Game map
+    private Deck deck; //Game deck
+
+    private MessageBox messageBox;
+
+    private int handSize;
+    private Location start;
+
+    public Game(int numPlayers, int handSize, int width, int height, Location start){
+
+        this.handSize = handSize;
+        this.start = start;
+
+        messageBox = new MessageBox();
+
+        players = new ArrayList<>();
+
+        for(int i=0; i<numPlayers; i++) {
+            Player player = new Player();
+
+            players.add(player);
+            messageBox.addObserver(player);
+        }
+
+        map = new Map(width, height, messageBox, players);
+        deck = new Deck(messageBox, players);
+    }
+
+    public void startGame(){
+
+        deck.shuffle(handSize);
+        map.initialize(start);
+    }
 }

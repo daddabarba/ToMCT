76d08d476143711e2e678ebc7f7296161346dc5a
daddabarba <davidebarbieri97@gmail.com>
2018-10-13 22:32:24 +0200

    location now paints if player is in there

--- src/ToMCT/View/GraphicComponents/ComponentsTypes/LocationPane.java
+++ src/ToMCT/View/GraphicComponents/ComponentsTypes/LocationPane.java
@@ -1,9 +1,12 @@
 package ToMCT.View.GraphicComponents.ComponentsTypes;
 
+import ToMCT.Model.ColoredTrails.Agent.Player;
 import ToMCT.Model.ColoredTrails.GameUtils.Location;
 import ToMCT.View.GraphicComponents.RelJComponent;
 
 import java.awt.*;
+import java.awt.font.TextAttribute;
+import java.text.AttributedString;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -11,6 +14,8 @@ public class LocationPane extends RelJComponent implements Observer {
 
     public static int EDGE_THICKNESS = MapPane.EDGE_THICKNESS;
     public static Color EDGE_COLOR = MapPane.EDGE_COLOR;
+
+    public static Color PLAYER_COLOR = Color.DARK_GRAY;
 
     private Location location;
 
@@ -22,17 +27,19 @@ public class LocationPane extends RelJComponent implements Observer {
     }
 
     @Override
-    public void PaintAbsElement(Graphics g, int x, int y, int height, int width){
+    public void PaintAbsElement(Graphics g, int x, int y, int height, int width) {
         //Make frame of map
+
+        //Paint frame
 
         //Set edge thickness
         Stroke oldStroke = null;
-        if(g instanceof Graphics2D) {
-            oldStroke =((Graphics2D) g).getStroke();
+        if (g instanceof Graphics2D) {
+            oldStroke = ((Graphics2D) g).getStroke();
             ((Graphics2D) g).setStroke(new BasicStroke(EDGE_THICKNESS));
         }
 
-        //Set color
+        //Set frame color
         Color oldColor = g.getColor();
         g.setColor(EDGE_COLOR);
 
@@ -40,13 +47,25 @@ public class LocationPane extends RelJComponent implements Observer {
         g.drawRect(x, y, width, height);
 
         //Reset edge thickness
-        if(oldStroke!=null)
+        if (oldStroke != null)
             ((Graphics2D) g).setStroke(oldStroke);
 
-        //Set color
+        //Paint filling
+
+        //Set filling color
         g.setColor(location.getTrail().color());
 
         g.fillRect(x, y, width, height);
+
+        //Paint player
+
+        if(location.getPlayers().size()>0) {
+            //Set player color
+            g.setColor(PLAYER_COLOR);
+
+            //Paint circle for player
+            g.fillOval(x+width/4, y+height/4, width/2, height/2);
+        }
 
         //Reset color
         g.setColor(oldColor);

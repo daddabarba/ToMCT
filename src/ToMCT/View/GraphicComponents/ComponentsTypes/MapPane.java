6fb2d1657af29d6ad59f3add678de7c74ed16dfb
daddabarba <davidebarbieri97@gmail.com>
2018-10-14 02:56:02 +0200

    changed edge color in map

--- src/ToMCT/View/GraphicComponents/ComponentsTypes/MapPane.java
+++ src/ToMCT/View/GraphicComponents/ComponentsTypes/MapPane.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class MapPane extends RelJComponent {
 
     public static int EDGE_THICKNESS = GamePane.EDGE_THICKNESS;
-    public static Color EDGE_COLOR = GamePane.EDGE_COLOR;
+    public static Color EDGE_COLOR = Color.LIGHT_GRAY;
 
     public MapPane(RelJComponent parent, Map map, double x, double y, double height, double width){
         super(parent, x, y, height, width);

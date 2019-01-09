011562be368c888d70fba5b18a5ba904c70d7c4f
daddabarba <davidebarbieri97@gmail.com>
2019-01-09 13:33:20 +0100

    ToM interface now has basic methods that all ToM agents should have

--- src/ToMCT/Model/ColoredTrails/Agent/ToM/ToM.java
+++ src/ToMCT/Model/ColoredTrails/Agent/ToM/ToM.java
@@ -1,4 +1,19 @@
 package ToMCT.Model.ColoredTrails.Agent.ToM;
 
+import ToMCT.Model.ColoredTrails.Agent.Player;
+import ToMCT.Model.ColoredTrails.GameTools.Chips.Offer;
+import ToMCT.Model.ColoredTrails.GameTools.Grid.Location;
+
 public interface ToM {
+
+    Offer ToM(Offer o, Player player);
+
+    double EV(Offer o, Player player);
+    double EV(Offer o);
+
+    double U(Offer o, Player player);
+    double U(Offer o);
+
+    double EV(Offer o, Location l, Player player);
+    double EV(Offer o, Location l);
 }

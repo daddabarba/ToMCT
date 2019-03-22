25ee2e12465c62870d2aaf027b424018ce5a83fb
daddabarba <davidebarbieri97@gmail.com>
2019-03-22 12:09:00 +0100

    added experiment script

---
+++ src/experiment.sh
@@ -0,0 +1,13 @@
+for ORDER1 in 0 1
+do
+	for ORDER2 in 0 1
+	do
+		for LR1 in $(seq 0.1 0.2 1.0)
+		do
+			for LR2 in $(seq 0.1 0.2 1.0)
+			do
+				./run.sh $ORDER1 $ORDER2 $LR1 $LR2 4 5 $1 false /home/daddabarba/Desktop/results/result &
+			done
+		done
+	done
+done

0f55c9965d82e9c8695fb3d125e39aa44a2cb785
D.Barbieri@student.rug.nl <s3090078@pg-interactive.hpc.rug.nl>
2019-06-19 13:56:55 +0200

    scratch version array job

--- src/experiment.sh
+++ src/experiment.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-#SBATCH --job-name=ToMCT_exp_$(( ( RANDOM % 10000 )  + 1 ))
 #SBATCH --mail-type=END
 #SBATCH --mail-user=davidebarbieri97@gmail.com
-#SBATCH --output=/home/s3090078/job-%j.log
+#SBATCH --output=/home/s3090078/results/job-%j.log
 
-#SBATCH --time=8-00:00:00
+#SBATCH --time=7-03:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1024
+#SBATCH --array=0-728
 
-srun ./run.sh $1 $2 $3 $4 4 5 $5 false
+order=({0..2})
+ls=($(seq 0.1 0.1 0.9))
+
+i=${SLURM_ARRAY_TASK_ID}
+ls2=$(($i%9))
+
+i=$i/9
+ls1=$(($i%9))
+
+i=$i/9
+o2=$(($i%3))
+
+i=$i/3
+o1=$(($i%3))
+
+
+srun ./run.sh $o1 $o2 $ls1 $ls2 4 5 $1 false

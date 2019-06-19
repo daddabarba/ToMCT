8463783033d590c0ce0c8c6cee104397111700a9
daddabarba <davidebarbieri97@gmail.com>
2019-06-19 14:04:08 +0200

    working script

--- src/experiment.sh
+++ src/experiment.sh
@@ -12,18 +12,12 @@
 
 order=({0..2})
 ls=($(seq 0.1 0.1 0.9))
-
 i=${SLURM_ARRAY_TASK_ID}
 ls2=$(($i%9))
-
 i=$i/9
 ls1=$(($i%9))
-
 i=$i/9
 o2=$(($i%3))
-
 i=$i/3
 o1=$(($i%3))
-
-
 srun ./run.sh $o1 $o2 $ls1 $ls2 4 5 $1 false

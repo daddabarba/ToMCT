35553ebb8f15f10343ec9a39753d887cb090b43c
daddabarba <davidebarbieri97@gmail.com>
2019-07-06 19:26:09 +0200

    addded code and script to generate plots

--- src/extract.py
+++ src/extract.py
@@ -2,6 +2,11 @@ import os
 import sys
 import functools as ftools
 
+import numpy as np
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
 import json
 
 def getFiles(path=os.getcwd()):
@@ -30,11 +35,13 @@ def refactorData(path=os.getcwd()):
 		try:
 			or0, or1, ls0, ls1, vals = getRes(file)
 			if (or0, or1, ls0, ls1) in refactored.keys():
-				print('\033[92m', "experiment ", (or0, or1, ls0, ls1) , " duplicate", '\033[0m')
+				if(len(sys.argv)<4 or sys.argv[4]!="false"):
+					print('\033[92m', "experiment ", (or0, or1, ls0, ls1) , " duplicate", '\033[0m')
 			else:
 				refactored[(or0, or1, ls0, ls1)] = vals
 		except:
-			print('\033[91m', "experiment ", file, " failed", '\033[0m')
+			if(len(sys.argv)<4 or sys.argv[4]!="false"):
+				print('\033[91m', "experiment ", file, " failed", '\033[0m')
 
 	return len(files), refactored
 
@@ -60,11 +67,57 @@ if __name__ == "__main__":
 	vals = data.keys()
 	vals = sorted(vals, key=ftools.cmp_to_key(compare))
 
+	avg = {}
+
 	for key in vals:
 		expdata = data[key]
 
+		a = np.array(expdata)
+		avg[key] = np.sum(a)/len(a)
+
 		n_neg = sum(x<0 for x in expdata)
 		n_pos = sum(x>0 for x in expdata)
-		print(key, " : ", n_neg, "/", n_pos)
+		# print(key, " : ", n_neg, "/", n_pos)
+		if(len(sys.argv)<4 or sys.argv[4]!="false"):
+			print(key, " : ", avg[key])
 
-	print(len(vals), "/", tot)
+	if(len(sys.argv)<4 or sys.argv[4]!="false"):
+		print(len(vals), "/", tot)
+
+
+	o1 = int(sys.argv[2])
+	o2 = int(sys.argv[3])
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+
+	# Grab some test data.
+	# X, Y, Z = axes3d.get_test_data(0.05)
+
+	# # Plot a basic wireframe.
+	# ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+
+	# plt.show()
+
+	X = np.zeros((11,11))
+	Z = np.zeros((11,11))
+
+	for i in range(0,11,1):
+		for k in range(0,11,1):
+			X[i][k] = i/10.0
+			idx = (o1,o2,i/10.0,k/10.0) 
+			if idx in avg.keys():
+				Z[i][k] = avg[idx]
+	Y = np.transpose(X)
+
+	ax.plot_surface(X, Y, Z, rstride=1, cstride=1)
+
+	plt.title(r"Delta payoff vs learning speed"
+           "\n"  # Newline: the backslash is interpreted as usual
+			  r"for order %d vs %d"%(o1,o2))
+
+	ax.set_xlabel("learning speed 1")
+	ax.set_ylabel("learning speed 1")
+	ax.set_zlabel("delta pyoff")
+
+	plt.savefig("data_"+str(o1)+"_"+str(o2)+".png")
---
+++ src/genPlots.sh
@@ -0,0 +1,7 @@
+for o1 in {0..2}
+do
+    for o2 in {0..2}
+    do
+        python3 extract.py $1 $o1 $o2 false
+    done
+done
